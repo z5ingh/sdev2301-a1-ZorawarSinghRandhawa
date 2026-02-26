2511eb699d010af4c8244c4e90bcdeacd1083d30
Zorawar Randhawa <zrandhawa1@nait.ca>
2026-02-26 09:50:37 -0700

    updated

--- Program.cs
+++ Program.cs
@@ -1,6 +1,57 @@
-﻿using sdev2301_a1_ZorawarSinghRandhawa.Data;
+﻿using sdev2301_a1_ZorawarSinghRandhawa.Services;
 
-using var db = new AppDbContext();
-db.Database.EnsureCreated();
+var studentService = new StudentService();
+var courseService = new CourseService();
 
-Console.WriteLine("Database created successfully!");
+while (true)
+{
+    Console.WriteLine("\n=== Student Enrollment System ===");
+    Console.WriteLine("1. Add Student");
+    Console.WriteLine("2. List Students");
+    Console.WriteLine("3. Add Course");
+    Console.WriteLine("4. List Courses");
+    Console.WriteLine("0. Exit");
+
+    Console.Write("Select option: ");
+    var choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            Console.Write("Enter student name: ");
+            var name = Console.ReadLine();
+
+            await studentService.AddAsync(name!, DateTime.Now);
+            Console.WriteLine("Student added!");
+            break;
+
+        case "2":
+            var students = await studentService.GetAllAsync();
+            foreach (var s in students)
+                Console.WriteLine($"{s.Id} - {s.FullName}");
+            break;
+
+        case "3":
+            Console.Write("Course code: ");
+            var code = Console.ReadLine();
+
+            Console.Write("Course name: ");
+            var cname = Console.ReadLine();
+
+            Console.Write("Credits: ");
+            var credits = int.Parse(Console.ReadLine()!);
+
+            await courseService.AddAsync(code!, cname!, credits);
+            Console.WriteLine("Course added!");
+            break;
+
+        case "4":
+            var courses = await courseService.GetAllAsync();
+            foreach (var c in courses)
+                Console.WriteLine($"{c.Id} - {c.Code} - {c.Name}");
+            break;
+
+        case "0":
+            return;
+    }
+}
