d7d43d2f5dc8614c7ab5809f612269d54ed9e722
Zorawar Randhawa <zrandhawa1@nait.ca>
2026-02-26 09:29:54 -0700

    started code

---
+++ Services/StudentService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sdev2301_a1_ZorawarSinghRandhawa.Data;
+using sdev2301_a1_ZorawarSinghRandhawa.Entities;
+
+namespace sdev2301_a1_ZorawarSinghRandhawa.Services;
+
+public class StudentService
+{
+    private readonly AppDbContext _db = new();
+
+   
