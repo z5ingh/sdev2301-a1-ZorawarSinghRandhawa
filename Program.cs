32db0d82aa54db954ead155c0e95b17da63268d5
Zorawar Randhawa <zrandhawa1@nait.ca>
2026-02-26 09:25:48 -0700

    database

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using sdev2301_a1_ZorawarSinghRandhawa.Data;
+
+using var db = new AppDbContext();
+db.Database.EnsureCreated();
+
+Console.WriteLine("Database created successfully!");
