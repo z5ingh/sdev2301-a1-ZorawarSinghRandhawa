323026f9191107cca379868645595ad4fcd2593a
Zorawar Randhawa <zrandhawa1@nait.ca>
2026-02-26 09:21:26 -0700

    course

---
+++ Entities/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace sdev2301_a1_ZorawarSinghRandhawa.Entities;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Code { get; set; } = "";
+    public string Name { get; set; } = "";
+    public int Credits { get; set; }
+
+    public ICollection<Student> Students { get; set; } = new List<Student>();
+}
