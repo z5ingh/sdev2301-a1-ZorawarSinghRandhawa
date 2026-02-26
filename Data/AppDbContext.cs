af969ab94f44d4d20a0a76e9795863a6093d87ed
Zorawar Randhawa <zrandhawa1@nait.ca>
2026-02-26 09:24:01 -0700

    appdbcontext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sdev2301_a1_ZorawarSinghRandhawa.Entities;
+
+namespace sdev2301_a1_ZorawarSinghRandhawa.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Student> Students => Set<Student>();
+    public DbSet<Course> Courses => Set<Course>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=school.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>()
+            .HasIndex(c => c.Code)
+            .IsUnique();
+    }
+}
